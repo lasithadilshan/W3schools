45aaaf3ac821f06c407c9c6aed7c799a5c3f9dd1
Lasitha Dilshan <dilshantilakaratne29@gmail.com>
2021-03-10 00:14:13 +0530

    Second Commit

--- app/src/main/java/com/dilshan/w3schools/MainActivity.java
+++ app/src/main/java/com/dilshan/w3schools/MainActivity.java
@@ -2,13 +2,17 @@ package com.dilshan.w3schools;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +20,11 @@ public class MainActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.webView);
 
-        
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.w3schools.com");
+
+        webView.setWebViewClient(new WebViewClient());
 
     }
 }
